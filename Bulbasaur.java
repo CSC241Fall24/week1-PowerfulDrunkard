5855f110441fe1afe31e1fa9560eaa841c2f0d56
PowerfulDrunkard <MS1031902@wcupa.edu>
2024-08-30 09:37:26 -0400

    completed week 1 assignments

---
+++ Bulbasaur.java
@@ -0,0 +1,69 @@
+public class Bulbasaur {
+    private int id;
+    private int level;
+
+    // Constructor
+    public Bulbasaur() {
+        id = 1;
+        level = 1;
+    }
+
+    // setLevel method
+    public void setLevel(int lv) {
+        // TODO: Implement this method
+        level = lv;
+        if(level >= 16 && level < 32){
+            id = 2;
+        }
+        else if(level >= 32){
+            id = 3;
+        }
+    }
+
+    // getLevel method
+    public int getLevel() {
+        // TODO: Implement this method
+        return level; // Placeholder return value
+    }
+
+    // getName method
+    public String getName() {
+        switch(id){
+            case 1: return "Bulbasaur";
+            case 2: return "Ivysaur";
+            case 3: return "Venasuar";
+        }
+        return "";
+    }
+
+    // getID method
+    public int getID() {
+        // TODO: Implement this method
+        return id; // Placeholder return value
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        // TODO: Implement this method
+        // Return a string representation of the Bulbasaur object
+        return "Level equals " + level + ". ID equals " + id; // Placeholder return value
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object obj) {
+        if(((Bulbasaur)obj).getID() == id &&
+        ((Bulbasaur)obj).getLevel() == level){
+            return true;
+        }
+        return false; // Placeholder return value
+    }
+
+    // copy method
+    public Bulbasaur copy() {
+        Bulbasaur clone = new Bulbasaur();
+        clone.setLevel(level);
+        return clone; // Placeholder return value
+    }
+}
